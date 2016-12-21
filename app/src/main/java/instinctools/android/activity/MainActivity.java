d6f7edfe16d953c28bb0e1b87e6e2fb7c6d5ac32
Orion <orion.gm@ya.ru>
2016-12-21 13:45:07 +0300

    save and restore state main activity after rotate device. and fix typo material design margin

--- app/src/main/java/instinctools/android/activity/MainActivity.java
+++ app/src/main/java/instinctools/android/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import instinctools.android.Book;
@@ -16,27 +17,47 @@ import instinctools.android.readers.FileReader;
 import instinctools.android.readers.JsonBookReader;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String BUNDLE_BOOKS = "BOOKS";
+
     private RecyclerView mRecyclerView;
     private BookAdapter mBookAdapter;
+    private List<Book> mBooks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(BUNDLE_BOOKS))
+                mBooks = savedInstanceState.getParcelableArrayList(BUNDLE_BOOKS);
+            else
+                readBooks();
+        } else
+            readBooks();
+
         initView();
+    }
+
+    private void readBooks() {
+        String str = new FileReader(this).read(R.raw.data);
+        mBooks = new JsonBookReader().read(str);
     }
 
     private void initView() {
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_book_list);
 
-        String str = new FileReader(this).read(R.raw.data);
-        List<Book> objects = new JsonBookReader().read(str);
-        mBookAdapter = new BookAdapter(this, mRecyclerView, objects);
+        mBookAdapter = new BookAdapter(this, mRecyclerView, mBooks);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
         mRecyclerView.setAdapter(mBookAdapter);
         mRecyclerView.addItemDecoration(new DividerItemDecoration(getResources().getDimensionPixelSize(R.dimen.recycler_item_child_layout_margin), ContextCompat.getDrawable(this, R.drawable.line_divider)));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (mBooks != null)
+            outState.putParcelableArrayList(BUNDLE_BOOKS, (ArrayList<Book>)mBooks);
     }
 }
