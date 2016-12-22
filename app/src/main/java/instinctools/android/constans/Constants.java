627df12825cc4a734a762dcd358d4bda064147d3
Orion <orion.gm@ya.ru>
2016-12-22 13:54:08 +0300

    implemented http client.

---
+++ app/src/main/java/instinctools/android/constans/Constants.java
@@ -0,0 +1,9 @@
+package instinctools.android.constans;
+
+/**
+ * Created by orion on 22.12.16.
+ */
+
+public class Constants {
+    public static String API_URL = "http://demo1569424.mockable.io/books";
+}
