350ae53ad673291468f48494ba436bb914094cf4
Orion <orion.gm@ya.ru>
2016-12-22 14:28:25 +0300

    implemented async loader

--- app/src/main/java/instinctools/android/activity/MainActivity.java
+++ app/src/main/java/instinctools/android/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package instinctools.android.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,12 +14,18 @@ import java.util.List;
 import instinctools.android.Book;
 import instinctools.android.R;
 import instinctools.android.adapters.BookAdapter;
+import instinctools.android.constans.Constants;
 import instinctools.android.decorations.DividerItemDecoration;
-import instinctools.android.readers.FileReader;
-import instinctools.android.readers.JsonBookReader;
+import instinctools.android.loaders.AsyncHttpLoader;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>> {
+    private static final String TAG = "MainActivity";
+
     private static final String BUNDLE_BOOKS = "BOOKS";
+
+    private static final int LOADER_CONTENT_ID = 1;
+
+    public static final String BUNDLE_LOADER_URL = "LOADER_URL";
 
     private RecyclerView mRecyclerView;
     private BookAdapter mBookAdapter;
@@ -28,36 +36,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState != null) {
-            if (savedInstanceState.containsKey(BUNDLE_BOOKS))
-                mBooks = savedInstanceState.getParcelableArrayList(BUNDLE_BOOKS);
-            else
-                readBooks();
-        } else
-            readBooks();
-
         initView();
-    }
 
-    private void readBooks() {
-        String str = new FileReader(this).read(R.raw.data);
-        mBooks = new JsonBookReader().read(str);
+        if (savedInstanceState == null || !savedInstanceState.containsKey(BUNDLE_BOOKS)) {
+            Bundle bundle = new Bundle();
+            bundle.putString(BUNDLE_LOADER_URL, Constants.API_URL);
+            getSupportLoaderManager().initLoader(LOADER_CONTENT_ID, bundle, this);
+        }
     }
 
     private void initView() {
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_book_list);
-
-        mBookAdapter = new BookAdapter(this, mRecyclerView, mBooks);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getResources().getDimensionPixelSize(R.dimen.recycler_item_child_layout_margin), ContextCompat.getDrawable(this, R.drawable.line_divider)));
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
-        mRecyclerView.setAdapter(mBookAdapter);
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(getResources().getDimensionPixelSize(R.dimen.recycler_item_child_layout_margin), ContextCompat.getDrawable(this, R.drawable.line_divider)));
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         if (mBooks != null)
-            outState.putParcelableArrayList(BUNDLE_BOOKS, (ArrayList<Book>)mBooks);
+            outState.putParcelableArrayList(BUNDLE_BOOKS, (ArrayList<Book>) mBooks);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState.containsKey(BUNDLE_BOOKS)) {
+            mBooks = savedInstanceState.getParcelableArrayList(BUNDLE_BOOKS);
+
+            mBookAdapter = new BookAdapter(this, mRecyclerView, mBooks);
+            mRecyclerView.setAdapter(mBookAdapter);
+        }
+    }
+
+    @Override
+    public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
+        Loader<List<Book>> loader = null;
+        if (id == LOADER_CONTENT_ID)
+            loader = new AsyncHttpLoader(this, args);
+
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> data) {
+        mBooks = data;
+
+        mBookAdapter = new BookAdapter(this, mRecyclerView, mBooks);
+        mRecyclerView.setAdapter(mBookAdapter);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
     }
 }
---
+++ app/src/main/java/instinctools/android/loaders/AsyncHttpLoader.java
@@ -0,0 +1,46 @@
+package instinctools.android.loaders;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.content.AsyncTaskLoader;
+
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import instinctools.android.Book;
+import instinctools.android.activity.MainActivity;
+import instinctools.android.network.HttpCustomClient;
+import instinctools.android.readers.JsonBookReader;
+
+/**
+ * Created by orion on 22.12.16.
+ */
+
+public class AsyncHttpLoader extends AsyncTaskLoader<List<Book>> {
+    private String mUrl = "http";
+
+    public AsyncHttpLoader(Context context, Bundle args) {
+        super(context);
+
+        if (args != null)
+            mUrl = args.getString(MainActivity.BUNDLE_LOADER_URL);
+    }
+
+    @Override
+    public List<Book> loadInBackground() {
+        HttpCustomClient client = new HttpCustomClient.Builder(mUrl).addHeader("Accept", "application/json").build();
+        client.sendRequest();
+        if (client.getResponseCode() != HttpURLConnection.HTTP_OK)
+            return new ArrayList<>();
+
+        List<Book> books = new JsonBookReader().read(client.getContent());
+        return books;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+}
